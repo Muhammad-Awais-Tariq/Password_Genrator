87650dfdc0901ce30d1656f0b163af14a4014b05
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-03-25 19:43:29 +0500

    Completed the programm

--- main.py
+++ main.py
@@ -17,5 +17,22 @@ def generate_password(min_len , want_digits = True , want_special = True):
         else:
             password += random.choice(letters)
 
-    print(password)
-generate_password(10 , True , False)
+    return password
+
+def get_info():
+    while True:
+        try:
+            min_len = int(input("Enter the minimum length of password that you need: "))
+            break
+        except ValueError:
+            print("Please enter the number")
+    want_digits = True if input("Want digits (Y /N): ").lower() == "y" else False
+    want_special = True if input("Want special chracters (Y /N): ").lower() == "y" else False
+    password = generate_password(min_len , want_digits , want_special)
+
+    print(f"Password: {password}")
+def main():
+    get_info()
+
+if __name__ == "__main__":
+    main()
